4c8e4f1695cecd74bfa88d4449f3cc278e3461df
Anton Veldanov <anton.veldanov@gmail.com>
2020-05-25 21:21:54 -0700

    iOS

--- FlickMapApp/MapViewController.swift
+++ FlickMapApp/MapViewController.swift
@@ -15,6 +15,8 @@ class MapViewController: UIViewController {
     // Do any additional setup after loading the view.
   }
 
-
+  @IBAction func centerMapButtonPressed(_ sender: UIButton) {
+  }
+  
 }
 
