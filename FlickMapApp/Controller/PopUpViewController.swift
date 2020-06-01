0beb7169f072e3f05c64ee52463a0fe824479a53
Anton Veldanov <anton.veldanov@gmail.com>
2020-06-01 13:33:19 -0700

    iOS

--- FlickMapApp/Controller/PopUpViewController.swift
+++ FlickMapApp/Controller/PopUpViewController.swift
@@ -9,24 +9,23 @@
 import UIKit
 
 class PopUpViewController: UIViewController {
-
+  var passedImage: UIImage?
+  
+  
+  
   @IBOutlet weak var popUpImageView: UIImageView!
+  
   
   override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
+  
+  func initData(forImage image: UIImage){
+    
+    passedImage = image
 
-    /*
-    // MARK: - Navigation
+  }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
