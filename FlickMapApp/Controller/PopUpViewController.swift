0694624345df2f7e4cdc8c18225e6265e47fb5b1
Anton Veldanov <anton.veldanov@gmail.com>
2020-06-01 13:24:42 -0700

    iOS

---
+++ FlickMapApp/Controller/PopUpViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PopUpViewController.swift
+//  FlickMapApp
+//
+//  Created by Veldanov, Anton on 6/1/20.
+//  Copyright © 2020 Anton Veldanov. All rights reserved.
+//
+
+import UIKit
+
+class PopUpViewController: UIViewController {
+
+  @IBOutlet weak var popUpImageView: UIImageView!
+  
+  override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
